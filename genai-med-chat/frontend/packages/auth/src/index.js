0afa1eddd2a1f68206e3c82ba999c93610938f1d
Suraj Shetty <shetty44444@gmail.com>
2025-11-11 15:08:13 +0530

    Delete genai-med-chat directory

--- genai-med-chat/frontend/packages/auth/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginForm } from './components/LoginForm';
-export { default as RegisterForm } from './components/RegisterForm';
-export { default as AuthProvider } from './context/AuthProvider';
-export { useAuth } from './context/AuthProvider';
--- genai-med-chat/frontend/packages/store/src/index.js
+++
@@ -1,3 +0,0 @@
-export { store } from './app/store';
-export * from './features/authSlice';
-export * from './features/chatSlice';
